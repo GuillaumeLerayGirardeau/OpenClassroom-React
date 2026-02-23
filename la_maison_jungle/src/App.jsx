7059d106638d869509e53ba9521486892288e2a7
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2026-02-23 16:16:43 +0100

    add: base files

---
+++ la_maison_jungle/src/App.jsx
@@ -0,0 +1,13 @@
+import Banner from './components/banner.jsx';
+import Cart from './components/cart.jsx';
+
+function App() {
+  return (
+  <div>
+    <Banner />
+    <Cart />
+  </div>
+  );
+}
+
+export default App;
