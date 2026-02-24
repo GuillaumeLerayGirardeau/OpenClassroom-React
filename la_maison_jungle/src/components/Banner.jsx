5df652a6472a3703fae90eba16f4a7ab02e18e3d
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2026-02-24 15:44:43 +0100

    add: logo added

--- la_maison_jungle/src/components/Banner.jsx
+++ la_maison_jungle/src/components/Banner.jsx
@@ -1,9 +1,15 @@
-import '../styles/banner.css'
+import '../styles/Banner.css'
+import logo from '../assets/logo.png'
 
 const Banner = () => {
   return (
-    <div className='lmj-banner'>
-        <h1>🌿 La maison jungle</h1>
+    <div className='banner'>
+      <img
+        src={logo}
+        alt="logo La maison jungle"
+        className='banner-logo'
+      />
+      <h1 className='banner-title'>La maison jungle</h1>
     </div>
   )
 }
